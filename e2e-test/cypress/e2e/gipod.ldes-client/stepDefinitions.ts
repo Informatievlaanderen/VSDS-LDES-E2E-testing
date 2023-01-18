874fee8fc4812bd6920fb023c1f20aed7cae35ac
Ranko Orlic <ranko@aquila-solutions.com>
2023-01-18 23:08:11 +0100

    chore: add example feature

---
+++ e2e-test/cypress/e2e/gipod.ldes-client/stepDefinitions.ts
@@ -0,0 +1,14 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given(/a Simulator-Workflow-Sink-Mongo context is started/, () => { return "pending"; });
+Given(/I have aliased the simulator's pre-seeded data set/, () => { return "pending"; });
+Given(/I have logged on to the Apache NiFi UI/, () => { return "pending"; });
+Given(/I have uploaded a workflow with an LDES client/, () => { return "pending"; });
+Given(/I have seeded the simulator with an initial data set/, () => { return "pending"; });
+Given(/I started the workflow/, () => { return "pending"; });
+Given(/the last fragment is being re-requested/, () => { return "pending"; });
+
+When(/I start the workflow/, () => { return "pending"; });
+When(/I seed a data set update/, () => { return "pending"; });
+
+Then(/the sink contains the correct number of members ({int})/, (count) => { return "pending"; });

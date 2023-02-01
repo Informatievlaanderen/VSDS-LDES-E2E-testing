76c8c076c3b4fa44361b1ddc83fe984efe83a3aa
Ruben Huybrighs <ruben.huybrighs@cegeka.com>
2023-02-01 16:52:25 +0100

    feat: add mongo rest api

---
+++ mongodb-rest-api/src/mongo-storage.ts
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+export class MongoStorage {
+    private _client: MongoClient;
+
+    public constructor(connectionUri: string) {
+        this._client = new MongoClient(connectionUri);
+    }
+
+    public async initialize() {
+        await this._client.connect();
+    }
+    public terminate() {
+        return this._client.close();
+    }
+
+    public async count(databaseName: string, collectionName: string): Promise<number> {
+        const count = await this._client.db(databaseName).collection(collectionName).estimatedDocumentCount({});
+        return count || 0;
+    }
+}

aaf42153310b99125f754fca2c241edf5e6fa3f6
Ruben Huybrighs <ruben.huybrighs@cegeka.com>
2023-02-02 14:42:02 +0100

    added LDES server GIPOD test 2 and 4

--- e2e-test/cypress/support/step_definitions/client_stepDefinitions.ts
+++ e2e-test/cypress/support/step_definitions/client_stepDefinitions.ts
@@ -1,40 +1,19 @@
-import { Given, When, Then, After, Before } from "@badeball/cypress-cucumber-preprocessor";
-import { credentials, DockerCompose, LdesServerSimulator, LdesWorkbenchNiFi } from '../../support'
-import { LdesClientSink } from "../../support/ldes-client-sink";
-
-const dockerCompose = new DockerCompose("support/context/simulator-workflow-sink-mongo/docker-compose.yml", {
-    USECASE_NAME: "gipod-replicate-and-sync-ldes",
-});
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { credentials, LdesServerSimulator, LdesWorkbenchNiFi } from '..'
+import { LdesClientSink } from "../ldes-client-sink";
 
 const apacheNifiUrl = 'https://localhost:8443';
 const simulatorUrl = 'http://localhost:9011';
 const sinkUrl = 'http://localhost:9003';
-
-before(() => {
-    dockerCompose.up(() => workbench.isReady());
-});
-
-afterEach(() => {
-    simulator.deleteFragments();
-    sink.deleteMember();
-});
-
-after(() => {
-    dockerCompose.down();
-});
-
 const simulator = new LdesServerSimulator(simulatorUrl);
 const workbench = new LdesWorkbenchNiFi(apacheNifiUrl)
 const sink = new LdesClientSink(sinkUrl);
 
-Given('a Simulator-Workflow-Sink-Mongo context is started', () => {
-    simulator.getAvailableFragmentsAndAliases().then(response => expect(response.isOkStatusCode).to.be.true);
-})
-
-Given('I have aliased the simulator\'s pre-seeded data set', () => {
+Given('I have aliased the {string} simulators pre-seeded data set', (testName: string) => {
+    simulator.isAvailable();
     simulator.seed(Cypress.env('gipodDataSet'));
-    simulator.postAlias('use-cases/gipod/1.replicate-ldes/create-alias.json');
-});
+    simulator.postAlias(`use-cases/gipod/${testName}/create-alias.json`);
+  })
 
 Given('I have logged on to the Apache NiFi UI', () => {
     workbench.logon(credentials);
@@ -59,6 +38,7 @@ Then('the sink contains {int} members', (count: number) => {
 })
 
 Given('I have seeded the simulator with an initial data set', () => {
+    simulator.isAvailable();
     ['alfa', 'beta'].forEach(baseName => simulator.postFragment(`use-cases/gipod/3.synchronize-ldes/data/${baseName}.jsonld`));
     simulator.postFragment('use-cases/gipod/3.synchronize-ldes/data/gamma.jsonld', 10);
     simulator.postAlias('use-cases/gipod/3.synchronize-ldes/create-alias.json');
@@ -71,10 +51,17 @@ Given('I started the workflow', () => {
 Given('the sync contains {int} members', (count: number) => {
     sink.checkCount('mobility-hindrances', count);
 })
+
 When('I seed a data set update', () => {
     simulator.postFragment('use-cases/gipod/3.synchronize-ldes/data/delta.jsonld', 10);
 })
 
 When('I seed another data set update', () => {
     simulator.postFragment('use-cases/gipod/3.synchronize-ldes/data/epsilon.jsonld', 10);
+})
+
+Given('I have uploaded the time-fragments data set', () => {
+    simulator.isAvailable();
+    ['alfa', 'beta', 'epsilon'].forEach(baseName => simulator.postFragment(`use-cases/gipod/4.time-fragment-ldes/data/scenario4/${baseName}.jsonld`));
+    simulator.postAlias('use-cases/gipod/4.time-fragment-ldes/create-alias.json');
 })
---
+++ e2e-test/cypress/support/step_definitions/step_definitions.ts
@@ -0,0 +1,17 @@
+import { After, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { DockerCompose } from "../docker-compose";
+import { LdesWorkbenchNiFi } from "../ldes-workbench-nifi";
+
+let dockerCompose : DockerCompose;
+export let dockerComposeEnvironment: {[key : string] : any} = {};
+const apacheNifiUrl = 'https://localhost:8443';
+const workbench = new LdesWorkbenchNiFi(apacheNifiUrl)
+
+Given('context {string} is started', (composeFile: string) => {
+    dockerCompose = new DockerCompose(`${composeFile}/docker-compose.yml`, dockerComposeEnvironment);
+    dockerCompose.up(() => workbench.isReady());
+})
+
+After(() => {
+    dockerCompose.down();
+});

da4dd53f7be3c359a36b37b8d9a26e68cbe5d60c
Ranko Orlic <ranko@aquila-solutions.com>
2023-04-11 21:35:08 +0200

    chore: fix tags and other minor issues

--- get-latest-docker-images.sh
+++ get-latest-docker-images.sh
@@ -5,16 +5,15 @@ docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:latest
 docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:latest
 docker pull ghcr.io/informatievlaanderen/ldes-server:latest
 
-# obsolete deliverables
+# historical deliverables
 docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:latest
-docker pull ghcr.io/informatievlaanderen/ldes-cli:latest
 
 # support
-docker pull ghcr.io/informatievlaanderen/test-message-generator:latest
-docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:latest
-docker pull ghcr.io/informatievlaanderen/test-message-sink:latest
 docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:latest
+docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:latest
 docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:latest
+docker pull ghcr.io/informatievlaanderen/test-message-generator:latest
+docker pull ghcr.io/informatievlaanderen/test-message-sink:latest
 
 # external
 docker pull ghcr.io/julianrojas87/gtfs2ldes-js:latest

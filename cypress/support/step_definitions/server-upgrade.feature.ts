30468b84119070d8dbdb5b99899315b1f22110f3
Ranko Orlic <ranko@aquila-solutions.com>
2023-04-17 11:32:50 +0200

    fix: wait differently to verify last fragment

--- cypress/support/step_definitions/server-upgrade.feature.ts
+++ cypress/support/step_definitions/server-upgrade.feature.ts
@@ -1,22 +1,27 @@
 import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
-import { LdesListFragments } from "../services";
-import { createAndStartService } from "./common_step_definitions";
+import { mongo } from "./common_step_definitions";
 import { Fragment } from "../ldes";
 
-const ldesListFragments = new LdesListFragments();
-let lastFragmentUrl;
 let lastMemberCount;
 
 When('I remember the last fragment member count', () => {
-    createAndStartService(ldesListFragments.serviceName)
-        .then(() => ldesListFragments.lastFragment().then(url => lastFragmentUrl = url)
-            .then(() => new Fragment(lastFragmentUrl).visit()
-                .then(fragment => cy.log(`Member count: ${fragment.memberCount}`).then(() => lastMemberCount = fragment.memberCount))));
+    mongo.fragments('iow_devices', 'ldesfragment')
+        .then(fragments => fragments.pop())
+        .then(partialUrl => new Fragment(`http://localhost:8080${partialUrl}`)
+            .visit()
+            .then(fragment => cy.log(`Member count: ${fragment.memberCount}`)
+            .then(() => lastMemberCount = fragment.memberCount))
+        );
 })
 
 Then('the last fragment member count increases', () => {
-    cy.waitUntil(() => new Fragment(lastFragmentUrl).visit()
-        .then(fragment => cy.log(`New member count: ${fragment.memberCount}`)
-            .then(() => fragment.memberCount > lastMemberCount)),
-            { timeout: 5000, interval: 1000 });
+    cy.waitUntil(() => 
+        mongo.fragments('iow_devices', 'ldesfragment')
+            .then(fragments => fragments.pop())
+            .then(partialUrl => new Fragment(`http://localhost:8080${partialUrl}`)
+                .visit()
+                .then(fragment => cy.log(`New member count: ${fragment.memberCount}`)
+                .then(() => fragment.memberCount > lastMemberCount))
+            ),
+        { timeout: 5000, interval: 1000 });
 })

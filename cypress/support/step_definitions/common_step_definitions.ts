472167a09e521749faa4f8a45e31fb3805895571
Ruben Huybrighs <ruben.huybrighs@cegeka.com>
2023-04-26 16:05:20 +0200

    Updated memory state, config path, added interface

--- cypress/support/step_definitions/common_step_definitions.ts
+++ cypress/support/step_definitions/common_step_definitions.ts
@@ -218,6 +218,10 @@ When('I remember the last fragment member count', () => {
         .then(fragment => cy.log(`Member count: ${fragment.memberCount}`).then(() => lastMemberCount = fragment.memberCount)));
 })
 
+When('I stop the LDIO workflow', () => {
+    dockerCompose.stop('ldi-orchestrator');
+})
+
 // Then stuff
 
 Then('the last fragment member count increases', () => {
@@ -251,5 +255,11 @@ Then('the LDES member count increases', () => {
 })
 
 Then('all but the first fragment have been requested once', () => {
-    
+    const firstFragmentUrl = "/api/v1/ldes/mobility-hindrances?generatedAtTime=2022-04-19T12:12:49.47Z";
+
+    simulator.getResponses().then(responses => {
+        expect(responses[firstFragmentUrl].count).to.be.equal(2);
+        delete responses[firstFragmentUrl];
+        Object.keys(responses).forEach(x => { expect(responses[x].count).to.be.equal(1); })
+    })
 })

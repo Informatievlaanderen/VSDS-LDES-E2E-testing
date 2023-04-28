b50091609d92389b58c6c5c6030c83c77c95a467
Ranko Orlic <ranko@aquila-solutions.com>
2023-04-28 13:15:40 +0200

    Tags/sprint 25 (#271)
    * fix: incompatible network options
    
    * chore: update tags

--- get-previous-docker-images.sh
+++ get-previous-docker-images.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # deliverables
-docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230407T130526
-docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230407T130529
-docker pull ghcr.io/informatievlaanderen/ldes-server:20230407T1308
+docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230424T152652
+docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230419T081024
+docker pull ghcr.io/informatievlaanderen/ldes-server:20230424T0954
 
 # historical deliverables
 docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230214T123440
 
 # support
-docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:20230411T1222
-docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:20230411T1225
-docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230411T0829
-docker pull ghcr.io/informatievlaanderen/test-message-generator:20230406T0837
-docker pull ghcr.io/informatievlaanderen/test-message-sink:20230411T0354
+docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:20230417T1447
+docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:20230417T1451
+docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230418T1946
+docker pull ghcr.io/informatievlaanderen/test-message-generator:20230421T1043
+docker pull ghcr.io/informatievlaanderen/test-message-sink:20230417T1510
 
 # external
 docker pull ghcr.io/julianrojas87/gtfs2ldes-js:20230306t2329
 docker pull mongo:6.0.5
-docker pull nginx:1.23.4
+docker pull nginx:1.24.0
 docker pull apache/nifi:1.21.0

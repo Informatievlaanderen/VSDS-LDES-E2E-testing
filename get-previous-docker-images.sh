ab372e825d602a626eae3e299726d4b3482317d7
Ranko Orlic <ranko@aquila-solutions.com>
2023-05-09 11:56:29 +0200

    Tags/sprint 26 (#277)
    * fix: updated tags for sprint 26
    
    * fix: updated tags

--- get-previous-docker-images.sh
+++ get-previous-docker-images.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # deliverables
-docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230424T152652
-docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230419T081024
-docker pull ghcr.io/informatievlaanderen/ldes-server:20230424T0954
+docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230508T085634
+docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230508T085639
+docker pull ghcr.io/informatievlaanderen/ldes-server:20230505T0853
 
 # historical deliverables
-docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230214T123440
+docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230412T141138
 
 # support
-docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:20230417T1447
-docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:20230417T1451
-docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230418T1946
-docker pull ghcr.io/informatievlaanderen/test-message-generator:20230421T1043
-docker pull ghcr.io/informatievlaanderen/test-message-sink:20230417T1510
+docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:20230502T1216
+docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:20230502T1216
+docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230502T1217
+docker pull ghcr.io/informatievlaanderen/test-message-generator:20230502T1217
+docker pull ghcr.io/informatievlaanderen/test-message-sink:20230502T1216
 
 # external
 docker pull ghcr.io/julianrojas87/gtfs2ldes-js:20230306t2329

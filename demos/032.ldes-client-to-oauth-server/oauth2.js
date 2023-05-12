f7f5c4bb8f104cf0d86b9d8f6835c7f7bbd0c6e1
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-12 16:51:54 +0200

    Fix setup

--- demos/032.ldes-client-to-oauth-server/oauth2.js
+++ demos/032.ldes-client-to-oauth-server/oauth2.js
@@ -1,21 +1,18 @@
 export default {introspectAccessToken}
 
 function introspectAccessToken(r) {
-  r.log("yay")
-  r.return(200)
-
   r.subrequest("/default/introspect",
-    // function (reply) {
-    //   if (reply.status !== 200) {
-    //     r.return(401); // Unexpected response, return 'auth required'
-    //   }
-    //
-    //   const response = JSON.parse(reply.responseBody);
-    //   if (response.active) {
-    //     r.return(204); // Token is valid, return success code
-    //   } else {
-    //     r.return(403); // Token is invalid, return forbidden code
-    //   }
-    // }
+    function (reply) {
+      if (reply.status !== 200) {
+        r.return(401); // Unexpected response, return 'auth required'
+      }
+
+      const response = JSON.parse(reply.responseBody);
+      if (response.active) {
+        r.return(204); // Token is valid, return success code
+      } else {
+        r.return(403); // Token is invalid, return forbidden code
+      }
+    }
   );
 }

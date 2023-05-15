ee5d568058b654e87a1920fa93e568f491b273e5
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-05-15 11:32:50 +0200

    Document nginx setup

--- tests/032.ldes-client-to-oauth-server/oauth2.js
+++ tests/032.ldes-client-to-oauth-server/oauth2.js
@@ -1,5 +1,12 @@
 export default {introspectAccessToken}
 
+/**
+ * A subrequest (mapped in the nginx config) is made to the oauth2 server.
+ * The response is either http 200 {active: false} or http 200 {active: true, ... other info ...}.
+ * This function translates the response json to an appropriate http response code (204, 401 or 403) which
+ * can be interpreted by the nginx 'auth_request' module.
+ * @param r nginx request object
+ */
 function introspectAccessToken(r) {
   r.subrequest("/default/introspect",
     function (reply) {

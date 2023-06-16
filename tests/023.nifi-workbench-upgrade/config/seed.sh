fbe37478805529d56aae82349018f7d1c5005250
Ranko Orlic <ranko@aquila-solutions.com>
2023-06-16 21:12:16 +0200

    fix: workbench upgrading tests

--- tests/023.nifi-workbench-upgrade/config/seed.sh
+++ tests/023.nifi-workbench-upgrade/config/seed.sh
@@ -6,9 +6,3 @@ code=$?
 if [ $code != 0 ] 
     then exit $code
 fi
-
-curl --fail -X POST 'http://localhost:8080/admin/api/v1/eventstreams/devices/views' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/devices.by-time.ttl"
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi

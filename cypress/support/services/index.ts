e4501b269e9918b38c0fa8c22c394351507a05f5
Ranko Orlic <ranko@aquila-solutions.com>
2023-06-20 10:50:04 +0200

    chore: some minor cleanup

--- cypress/support/services/index.ts
+++ cypress/support/services/index.ts
@@ -3,6 +3,7 @@ export * from './ldes-server';
 export * from './ldes-server-simulator';
 export * from './ldes-workbench-ldio';
 export * from './ldes-workbench-nifi';
+export * from './member-generator';
 export * from './mongo-rest-api';
 export * from './test-message-generator';
 export * from './test-message-sink';

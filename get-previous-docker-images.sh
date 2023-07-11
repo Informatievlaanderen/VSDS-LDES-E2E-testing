7bcbc8dab91e8d1c482ac9aae2b2a28068e81aee
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-11 21:39:55 +0200

    chore: update image tags

--- get-previous-docker-images.sh
+++ get-previous-docker-images.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # deliverables
-docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230508T085634
-docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230508T085639
-docker pull ghcr.io/informatievlaanderen/ldes-server:20230505T0853
+docker pull ghcr.io/informatievlaanderen/ldi-workbench-nifi:20230704080200
+docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230704080159
+docker pull ghcr.io/informatievlaanderen/ldes-server:20230703153349
 
 # historical deliverables
 docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230412T141138
@@ -11,12 +11,12 @@ docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230412T141138
 # support
 docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:20230502T1216
 docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:20230502T1216
-docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230502T1217
+docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:20230629T1832
 docker pull ghcr.io/informatievlaanderen/test-message-generator:20230502T1217
 docker pull ghcr.io/informatievlaanderen/test-message-sink:20230502T1216
 
 # external
-docker pull ghcr.io/julianrojas87/gtfs2ldes-js:20230306t2329
+docker pull ghcr.io/julianrojas87/gtfs2ldes-js:20230306T2329
 docker pull mongo:6.0.5
 docker pull nginx:1.24.0
 docker pull apache/nifi:1.21.0

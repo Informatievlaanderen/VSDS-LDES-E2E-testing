e5865da4cf0de5a5031da2595f230671f049036a
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-13 21:10:25 +0200

    fix: script header

--- tests/032.ldes-client-to-oauth-server/config/seed.sh
+++ tests/032.ldes-client-to-oauth-server/config/seed.sh
@@ -1,6 +1,5 @@
-#!/bin/#!/bin/bash
+#!/bin/bash
 export SCRIPT_PATH=$(dirname -- "$( readlink -f -- "${BASH_SOURCE:-$0}"; )")
-
 
 curl --fail -X POST 'http://localhost:8081/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/devices.ttl"
 code=$?

53fbad5ae00be2e4e6d0e82516198df4f2352ba7
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-25 08:46:08 +0200

    fix: add TZ for linux

--- tests/012.server-multiple-retention-policies/config/future-date.sh
+++ tests/012.server-multiple-retention-policies/config/future-date.sh
@@ -3,7 +3,7 @@ export SCRIPT_PATH=$(dirname -- "$( readlink -f -- "${BASH_SOURCE:-$0}"; )")
 OS=$($SCRIPT_PATH/detect-os.sh)
 
 if [[ "$OS" == "linux" ]]; then
-    echo $(date -u -d "$@ seconds" +"%Y-%m-%dT%H:%M:%S");
+    echo $(date -u -d "$@ seconds" +"%Y-%m-%dT%H:%M:%S%:z");
 elif [[ "$OS" == "mac" ]]; then
     echo $(date -u -Iseconds -v +$@S);
 elif [[ "$OS" == "windows" ]]; then

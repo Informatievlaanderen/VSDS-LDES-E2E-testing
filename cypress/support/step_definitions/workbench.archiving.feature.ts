3da9fa255d369faf12589923e97d8be153b5e727
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-27 12:39:54 +0200

    wip

--- cypress/support/step_definitions/workbench.archiving.feature.ts
+++ cypress/support/step_definitions/workbench.archiving.feature.ts
@@ -10,7 +10,7 @@ import { checkSuccess } from "..";
 
 const createLdioWorkbench = new LdesWorkbenchLdio(undefined, 'ldio-create-archive');
 const readLdioWorkbench = new LdesWorkbenchLdio(undefined, 'ldio-read-archive');
-const archiveFolder = 'data/archive';
+let archiveFolder: string;
 
 Given('I have configured the archive directory', () => {
     if (Cypress.platform === 'linux') {
@@ -19,7 +19,10 @@ Given('I have configured the archive directory', () => {
             checkSuccess(result).then(success => expect(success).to.be.true);
         })
     }
-    setAdditionalEnvironmentSetting('ARCHIVE_DIR', `../../${archiveFolder}`);
+    //setAdditionalEnvironmentSetting('ARCHIVE_DIR', `../../${archiveFolder}`);
+
+    // cy.exec('docker volume inspect --format {{.Mountpoints}} ldes-data-archive')
+    //     .then(result => archiveFolder = result.stdout);
 })
 
 When('I start the create archive {string} workbench', (workbench: string) => {
@@ -69,5 +72,11 @@ Then('I wait until the {string} workbench finished archiving', (workbench: strin
 
 Then('I cleanup the created archive', () => {
     cy.exec(`rm -rf ./${archiveFolder}/2022`)
-        .then(result => checkSuccess(result).then(success => expect(success).to.be.true));
+        .then(result => checkSuccess(result).then(success => expect(success).to.be.true))
+
+    // // re-create data archive volume
+    // cy.exec('docker volume rm ldes-data-archive')
+    //     .then(result => checkSuccess(result).then(success => expect(success).to.be.true))
+    //     .then(() => cy.exec('docker volume create ldes-data-archive')
+    //         .then(result => checkSuccess(result).then(success => expect(success).to.be.true)));
 })

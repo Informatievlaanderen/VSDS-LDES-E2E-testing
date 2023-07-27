bb886e4ffc123859d6da8e49f364c98853df72ee
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-27 17:31:56 +0200

    Fix/test 033 (#352)
    * fix: create subfolders and cleanup at end
    
    * chore: update cypress

--- init-test-context.sh
+++ init-test-context.sh
@@ -3,8 +3,10 @@
 # allow full access to the EPSG
 sudo mkdir -p -m 0777 ./data/epsg
 
-# create archiving volume (if it does not yet exist) and allow full access to it
-sudo mkdir -p -m 0777 ./data/archive
+# create archiving folders and set access rights
+sudo mkdir -p ./data/archive
+sudo mkdir -p -m 0777 ./data/archive/ldio
+sudo mkdir -p -m 0777 ./data/archive/nifi
 
 # additionally allow full access to test 006 node-red folder
 sudo chmod 0777 ./tests/006.server-substring-fragment-ldes/data/node-red

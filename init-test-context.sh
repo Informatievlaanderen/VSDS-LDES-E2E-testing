ee745846407869d3fedd19d13be4bf8fa1d161fd
Ranko Orlic <ranko@aquila-solutions.com>
2023-07-27 17:46:19 +0200

    fix: run script as sudo

--- init-test-context.sh
+++ init-test-context.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # allow full access to the EPSG
-sudo mkdir -p -m 0777 ./data/epsg
+mkdir -p -m 0777 ./data/epsg
 
 # create archiving folders and set access rights
-sudo mkdir -p ./data/archive
-sudo mkdir -p -m 0777 ./data/archive/ldio
-sudo mkdir -p -m 0777 ./data/archive/nifi
+mkdir -p ./data/archive
+mkdir -p -m 0777 ./data/archive/ldio
+mkdir -p -m 0777 ./data/archive/nifi
 
 # additionally allow full access to test 006 node-red folder
-sudo chmod 0777 ./tests/006.server-substring-fragment-ldes/data/node-red
+chmod 0777 ./tests/006.server-substring-fragment-ldes/data/node-red

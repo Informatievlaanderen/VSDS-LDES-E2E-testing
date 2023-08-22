2ba6c67a0ea00688a238b556802c82dcf36e3021
Ranko Orlic <ranko@aquila-solutions.com>
2023-08-22 07:49:11 +0200

    chore: add docker hub images

---
+++ get-hub-docker-images.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+export LDI_WORKBENCH_NIFI=docker.io/ldes/ldi-workbench-nifi
+export LDI_WORKBENCH_NIFI_TAG=1.4.0-SNAPSHOT
+export LDI_ORCHESTRATOR=docker.io/ldes/ldi-orchestrator
+export LDI_ORCHESTRATOR_TAG=1.4.0-SNAPSHOT
+export LDES_SERVER=docker.io/ldes/ldes-server
+export LDES_SERVER_TAG=2.0.0-SNAPSHOT
+
+# deliverables
+docker pull $LDI_WORKBENCH_NIFI:$LDI_WORKBENCH_NIFI_TAG
+docker pull $LDI_ORCHESTRATOR:$LDI_ORCHESTRATOR_TAG
+docker pull $LDES_SERVER:$LDES_SERVER_TAG
+
+# historical deliverables (for upgrade tests)
+docker pull ghcr.io/informatievlaanderen/ldes-workbench-nifi:20230124T140316
+docker pull ghcr.io/informatievlaanderen/ldi-orchestrator:20230324T175226
+docker pull ghcr.io/informatievlaanderen/ldes-server:20230405T0843
+
+# support
+docker pull ghcr.io/informatievlaanderen/ldes-list-fragments:latest
+docker pull ghcr.io/informatievlaanderen/ldes-server-simulator:latest
+docker pull ghcr.io/informatievlaanderen/mongodb-rest-api:latest
+docker pull ghcr.io/informatievlaanderen/test-message-generator:latest
+docker pull ghcr.io/informatievlaanderen/test-message-sink:latest
+
+# external
+docker pull ghcr.io/julianrojas87/gtfs2ldes-js:latest
+docker pull mongo:latest
+docker pull nginx:latest
+docker pull apache/nifi:latest
+docker pull ghcr.io/navikt/mock-oauth2-server:1.0.0

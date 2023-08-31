ef6daa80a505201fe4b81dc1003b9080f6238640
Ranko Orlic <ranko@aquila-solutions.com>
2023-08-31 06:30:33 +0200

    chore: bumped versions

--- get-hub-docker-images.sh
+++ get-hub-docker-images.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 export LDI_WORKBENCH_NIFI=docker.io/ldes/ldi-workbench-nifi
-export LDI_WORKBENCH_NIFI_TAG=1.4.0-SNAPSHOT
+export LDI_WORKBENCH_NIFI_TAG=1.5.0-SNAPSHOT
 export LDI_ORCHESTRATOR=docker.io/ldes/ldi-orchestrator
-export LDI_ORCHESTRATOR_TAG=1.4.0-SNAPSHOT
+export LDI_ORCHESTRATOR_TAG=1.5.0-SNAPSHOT
 export LDES_SERVER=docker.io/ldes/ldes-server
-export LDES_SERVER_TAG=2.0.0-SNAPSHOT
+export LDES_SERVER_TAG=2.0.1-SNAPSHOT
 
 # deliverables
 docker pull $LDI_WORKBENCH_NIFI:$LDI_WORKBENCH_NIFI_TAG

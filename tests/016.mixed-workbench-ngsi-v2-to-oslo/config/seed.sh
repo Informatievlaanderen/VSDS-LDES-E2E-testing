13e1eb3027568d057598321d54371bf7b47f114b
pj-cegeka <pieterjan.livens@cegeka.com>
2024-07-03 12:23:18 +0200

    fix: remove delete view from seed scripts

--- tests/016.mixed-workbench-ngsi-v2-to-oslo/config/seed.sh
+++ tests/016.mixed-workbench-ngsi-v2-to-oslo/config/seed.sh
@@ -7,12 +7,6 @@ if [ $code != 0 ]
     then exit $code
 fi
 
-curl -X DELETE 'http://localhost:8080/admin/api/v1/eventstreams/device-models/views/by-page'
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi
-
 curl --fail -X POST 'http://localhost:8080/admin/api/v1/eventstreams/device-models/views' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/device-models.by-page.ttl"
 code=$?
 if [ $code != 0 ] 
@@ -25,12 +19,6 @@ if [ $code != 0 ]
     then exit $code
 fi
 
-curl -X DELETE 'http://localhost:8080/admin/api/v1/eventstreams/devices/views/by-page'
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi
-
 curl --fail -X POST 'http://localhost:8080/admin/api/v1/eventstreams/devices/views' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/devices.by-page.ttl"
 code=$?
 if [ $code != 0 ] 
@@ -38,12 +26,6 @@ if [ $code != 0 ]
 fi
 
 curl --fail -X POST 'http://localhost:8080/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/water-quality-observations.ttl"
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi
-
-curl -X DELETE 'http://localhost:8080/admin/api/v1/eventstreams/water-quality-observations/views/by-page'
 code=$?
 if [ $code != 0 ] 
     then exit $code
--- tests/032.ldes-client-to-oauth-server/config/seed.sh
+++ tests/032.ldes-client-to-oauth-server/config/seed.sh
@@ -7,12 +7,6 @@ if [ $code != 0 ]
     then exit $code
 fi
 
-curl -X DELETE 'http://localhost:8081/admin/api/v1/eventstreams/devices/views/by-page'
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi
-
 curl --fail -X POST 'http://localhost:8081/admin/api/v1/eventstreams/devices/views' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/devices.by-page.ttl"
 code=$?
 if [ $code != 0 ] 

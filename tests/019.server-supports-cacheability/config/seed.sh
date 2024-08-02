3b7a06241ad3ce0e0259de5494b88dd9ca1a62b2
Ranko Orlic <ranko@aquila-solutions.com>
2024-08-02 21:37:40 +0200

    fix: remove by-page delete

--- tests/019.server-supports-cacheability/config/seed.sh
+++ tests/019.server-supports-cacheability/config/seed.sh
@@ -7,12 +7,6 @@ if [ $code != 0 ]
     then exit $code
 fi
 
-curl -X DELETE 'http://localhost:8082/admin/api/v1/eventstreams/occupancy/views/by-page'
-code=$?
-if [ $code != 0 ] 
-    then exit $code
-fi
-
 curl --fail -X POST 'http://localhost:8082/admin/api/v1/eventstreams/occupancy/views' -H 'Content-Type: text/turtle' -d "@$SCRIPT_PATH/occupancy.by-page.ttl"
 code=$?
 if [ $code != 0 ] 

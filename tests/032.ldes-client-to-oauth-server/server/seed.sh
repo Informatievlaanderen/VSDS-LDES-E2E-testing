30dcc4e0890061b5368c7cfe6ef5b96defb730b0
Ranko Orlic <ranko@aquila-solutions.com>
2024-08-06 08:15:42 +0200

    chore: fix some tests

---
+++ tests/032.ldes-client-to-oauth-server/server/seed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export SCRIPT_PATH=$(dirname -- "$( readlink -f -- "${BASH_SOURCE:-$0}"; )")
+export LDES_PATH=$SCRIPT_PATH/ldes
+
+curl --fail -X POST 'http://localhost:8082/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d "@$LDES_PATH/devices.ttl"
+code=$?
+if [ $code != 0 ] 
+    then exit $code
+fi
+
+curl --fail -X POST 'http://localhost:8082/admin/api/v1/eventstreams/devices/views' -H 'Content-Type: text/turtle' -d "@$LDES_PATH/devices.by-page.ttl"
+code=$?
+if [ $code != 0 ] 
+    then exit $code
+fi

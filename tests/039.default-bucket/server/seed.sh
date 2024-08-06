74bdc9c2040b5a3f7257df372d62c449747317d3
Ranko Orlic <ranko@aquila-solutions.com>
2024-08-06 20:15:03 +0200

    fix: move members

--- tests/039.default-bucket/server/seed.sh
+++ tests/039.default-bucket/server/seed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export SCRIPT_PATH=$(dirname -- "$( readlink -f -- "${BASH_SOURCE:-$0}"; )")
 export LDES_PATH=$SCRIPT_PATH/ldes
+export DATA_PATH=$SCRIPT_PATH/$members
 
 curl --fail -X POST 'http://localhost:8080/admin/api/v1/eventstreams' -H 'Content-Type: text/turtle' -d "@$LDES_PATH/ldes.ttl"
 code=$?
@@ -32,7 +33,7 @@ fi
 i=0
 while [ $i -lt 5 ];
 do
-    curl --fail -X POST 'http://localhost:8080/mobility-hindrances' -H 'Content-Type: text/turtle' -d "@$LDES_PATH/members/member$i.ttl"
+    curl --fail -X POST 'http://localhost:8080/mobility-hindrances' -H 'Content-Type: text/turtle' -d "@$DATA_PATH/member$i.ttl"
     code=$?
     i=$((i+1))
     if [ $code != 0 ]

f8649b0de04998a7ceda94589763ce3dfe189dd3
Ranko Orlic <ranko@aquila-solutions.com>
2024-08-13 14:20:40 +0200

    chore: manual test for RepoSink skolem IDs

--- tests/040.skolemization/run-test.sh
+++ tests/040.skolemization/run-test.sh
@@ -6,7 +6,6 @@ curl -X POST "http://localhost:8080/observations" -H "content-type: text/turtle"
 
 # get page (as N-triples) -- wait until page fragmented
 COUNT=0 && while [ "$COUNT" -lt "99" ] ; do sleep 3; COUNT=$(curl -s -H "accept: application/n-triples" "http://localhost:8080/observations/by-page?pageNumber=1" --output page.nt && cat page.nt | wc -l); echo "count: $COUNT" ; done
-cat page.nt
 
 # check that all subjects are named nodes (and as such no blank nodes exist)
 NAMED_NODE_COUNT=$(cat page.nt | grep -e '^<http' | wc -l)
@@ -26,26 +25,37 @@ echo "Skolem ID count $SKOLEM_ID_COUNT matches unique count $UNIQUE_ID_COUNT, th
 # start the LDES client workflow
 curl -X POST http://localhost:8081/admin/api/v1/pipeline -H "Content-Type: application/yaml" --data-binary "@./workbench/client-pipeline.yml"
 
-# query initial skolem IDs
-COUNT=0 && while [ "$COUNT" -lt "1" ] ; do sleep 3; COUNT=$(curl -s -H "accept: application/sparql-results+json" http://localhost:7200/repositories/observations/size); echo "count: $COUNT" ; done
-OBSERVATION1=$(export OBSERVATION_URI="<https://example.org/id/observation/1>" && envsubst < ./graphdb/query.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
-OBSERVATION2=$(export OBSERVATION_URI="<https://example.org/id/observation/2>" && envsubst < ./graphdb/query.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+# wait until members received
+INITIAL_COUNT=0 && while [ "$INITIAL_COUNT" -lt "92" ] ; do sleep 3; INITIAL_COUNT=$(curl -s -H "accept: application/sparql-results+json" http://localhost:7200/repositories/observations/size); echo "count: $INITIAL_COUNT" ; done
 
-# send member 1 update
+# query initial skolem IDs
+OBSERVATION1=$(export OBSERVATION_URI="<https://example.org/id/observation/1>" && envsubst < ./graphdb/skolem-ids.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+OBSERVATION2=$(export OBSERVATION_URI="<https://example.org/id/observation/2>" && envsubst < ./graphdb/skolem-ids.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+
+# send member update
+sleep 1
 curl -X POST "http://localhost:8080/observations" -H "content-type: text/turtle" --data-binary @./data/members.update.ttl
+
+# wait until member update is processed
+UNIQUE_DATE_COUNT=0 && while [ "$UNIQUE_DATE_COUNT" -ne "2" ] ; do sleep 3; UNIQUE_DATE_COUNT=$(curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @./graphdb/generated-at-dates.rq | jq -c '.results.bindings[].date.value' | uniq | wc -l) ; echo "unique number of dates: $UNIQUE_DATE_COUNT" ; done
 echo ""
 
 # query skolem IDs after update
-COUNT=0 && while [ "$COUNT" -lt "131" ] ; do sleep 3; COUNT=$(curl -s -H "accept: application/sparql-results+json" http://localhost:7200/repositories/observations/size); echo "count: $COUNT" ; done
-UPDATED1=$(export OBSERVATION_URI="<https://example.org/id/observation/1>" && envsubst < ./graphdb/query.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
-UPDATED2=$(export OBSERVATION_URI="<https://example.org/id/observation/2>" && envsubst < ./graphdb/query.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+UPDATED1=$(export OBSERVATION_URI="<https://example.org/id/observation/1>" && envsubst < ./graphdb/skolem-ids.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+UPDATED2=$(export OBSERVATION_URI="<https://example.org/id/observation/2>" && envsubst < ./graphdb/skolem-ids.rq | curl -s -X POST -H "Content-Type: application/sparql-query" -H "accept: application/sparql-results+json" "http://localhost:7200/repositories/observations" -d @-)
+
+# expect triple size to be the same
+UPDATED_COUNT=$(curl -s -H "accept: application/sparql-results+json" http://localhost:7200/repositories/observations/size)
+if [ "$INITIAL_COUNT" != "$UPDATED_COUNT" ] 
+  then echo "Initial triple count ($INITIAL_COUNT) does not match updated triple count ($UPDATED_COUNT), therefore not all skolem triples were deleted" && exit -1
+fi
 
 # expect all skolem IDs for observation 1 are different
 readarray -t SKOLEM_IDS1 < <(echo $OBSERVATION1 | jq -c '.results.bindings[].bs.value' | sort)
 readarray -t UPDATED_IDS1 < <(echo $UPDATED1 | jq -c '.results.bindings[].bs.value' | sort)
 for ((i=0; i<${#UPDATED_IDS1[@]}; ++i)); do
   if [ "${SKOLEM_IDS1[i]}" == "${UPDATED_IDS1[i]}" ] 
-    then echo "Original (${SKOLEM_IDS1[i]}) and updated (${UPDATED_IDS1[i]}) skolem ID match for member 1 (at index $i), therefore skolem IDs were not correctly deleted before updating the member" && exit -1
+    then echo "Initial (${SKOLEM_IDS1[i]}) and updated (${UPDATED_IDS1[i]}) skolem ID match for member 1 (at index $i), therefore skolem IDs were not correctly deleted before updating the member" && exit -1
   fi
 done
 echo "All skolem IDs for member 1 are different"
@@ -55,10 +65,9 @@ readarray -t SKOLEM_IDS2 < <(echo $OBSERVATION2 | jq -c '.results.bindings[].bs.
 readarray -t UPDATED_IDS2 < <(echo $UPDATED2 | jq -c '.results.bindings[].bs.value' | sort)
 for ((i=0; i<${#UPDATED_IDS2[@]}; ++i)); do
   if [ "${SKOLEM_IDS2[i]}" != "${UPDATED_IDS2[i]}" ] 
-    then echo "Original (${SKOLEM_IDS2[i]}) and updated (${UPDATED_IDS2[i]}) skolem ID do not match for member 2 (at index $i), therefore skolem IDs were incorrectly deleted when updating the other member" && exit -1
+    then echo "Initial (${SKOLEM_IDS2[i]}) and updated (${UPDATED_IDS2[i]}) skolem ID do not match for member 2 (at index $i), therefore skolem IDs were incorrectly deleted when updating the other member" && exit -1
   fi
 done
 echo "All skolem IDs for member 2 are identical"
 
 echo "All checks passed."
-
